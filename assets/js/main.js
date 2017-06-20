56cb736a35cb302aecebd7fb2ef63e4bc4072305
karina palacios <karina.spalacios@gmail.com>
2017-06-20 19:23:22 -0400

    menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-/* Anda a laaar xD */
+      // When your page loads
+      $(function(){
+         // When the toggle areas in your navbar are clicked, toggle them
+         $("#search-button, #search-icon").click(function(e){
+             e.preventDefault();
+             $("#search-button, #search-form").toggle();
+         });
+      })
